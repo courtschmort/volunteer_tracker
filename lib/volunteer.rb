e9606fc60d50383b9e9756fec5b3f7d44128fbe9
Courtney Schild <courtneyschild@gmail.com>
2020-01-10 13:35:40 -0800

    add .all() method to pass Volunteer tests 'is empty to start' and 'returns all volunteers'.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -21,6 +21,18 @@ class Volunteer
     @id = db_volunteers.first().fetch("id").to_i
   end
 
+  def self.all
+    db_volunteers= DB.exec('SELECT * FROM volunteers;')
+    volunteers =[]
+    db_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
+
   def self.find_by_project(proj_id)
     db_volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{proj_id};")
     volunteers = []
