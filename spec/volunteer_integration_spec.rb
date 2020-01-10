d12f9328b915fe22ac35661568b9c9cc8a16253d
Courtney Schild <courtneyschild@gmail.com>
2020-01-10 14:25:56 -0800

    add integration spec 'takes the user to the homepage where they can create a project'.

--- spec/volunteer_integration_spec.rb
+++ spec/volunteer_integration_spec.rb
@@ -1,4 +1,20 @@
 require 'capybara/rspec'
+require 'pry'
 require './app'
+require 'spec_helper'
+
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+# Your project should be set up so that a volunteer can only be created if a project already exists. (This makes it easier to assign the one to many relationship in Sinatra.) Focus on getting one integration spec passing at a time.
+
+# The user should be able to visit the home page and fill out a form to add a new project. When that project is created, the application should direct them back to the homepage.
+
+describe 'the project creation path', {:type => :feature} do
+  it 'takes the user to the homepage where they can create a project' do
+    visit '/'
+    fill_in('title', :with => 'Teaching Kids to Code')
+    click_button('Create Project')
+    expect(page).to have_content('Teaching Kids to Code')
+  end
+end
