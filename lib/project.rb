4fab2fd58612f996dd4675867aba664f69306e96
Courtney Schild <courtneyschild@gmail.com>
2020-01-10 12:24:49 -0800

    add .all() method and redefine == to pass Project test 'saves a project to the database'.

--- lib/project.rb
+++ lib/project.rb
@@ -12,4 +12,19 @@ class Project
     @id = db_projects.first().fetch('id').to_i
   end
 
+  def self.all
+    db_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    db_projects.each() do |project|
+      title = project.fetch('title')
+      id = project.fetch('id').to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
+  def ==(project_to_compare)
+    self.title == project_to_compare.title
+  end
+
 end
