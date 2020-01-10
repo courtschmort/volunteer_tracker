515e6c8ca1dbb360f19e4517ef486fe71dfef4a3
Courtney Schild <courtneyschild@gmail.com>
2020-01-10 10:08:28 -0800

    add initial content to lib files.

---
+++ lib/project.rb
@@ -0,0 +1,10 @@
+class Project
+  attr_accessor :name
+  attr_reader :id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+end
---
+++ lib/volunteer.rb
@@ -0,0 +1,11 @@
+class Volunteer
+  attr_accessor :name, :project_id
+  attr_reader :id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
+
+end
